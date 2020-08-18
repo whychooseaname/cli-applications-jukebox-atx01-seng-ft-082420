03aee54b75e83c184c2be300e62428efba8adf18
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-18 05:52:08 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -23,11 +23,16 @@ songs = [
 
 
 def help 
-  puts 
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
   
 end
 
 def play 
+  puts songs
   
 end
 
