320d545c09432496edc6aee79cbd0d1fdf456456
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-18 06:39:58 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -34,10 +34,10 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   song_name = gets.strip
-  songs.each.with_index(1) do |song, track|
+  songs.each do |song, track|
     if song_name.to_i 
       puts "Playing #{songs[song_name.to_i - 1]}"
-    elsif song_name == song 
+    elsif song_name == track 
       puts "Playing #{song_name}"
     else 
       puts "Invalid input, please try again"
