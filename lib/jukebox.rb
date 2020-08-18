d304b231e13c6b568faceb98c7d2988992750cb2
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-18 05:49:50 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,10 +1,26 @@
 # Add your code here
 
-def say_hello(name)
-  "Hi #{name}!"
+#def say_hello(name)
+#  "Hi #{name}!"
+#end
+ 
+#puts "Enter your name:"
+#users_name = gets.strip
+ 
+#puts say_hello(users_name)
+
+def help 
+  
 end
- 
-puts "Enter your name:"
-users_name = gets.strip
- 
-puts say_hello(users_name)
+
+def play 
+  
+end
+
+def list 
+  
+end
+
+def exit_jukebox
+  
+end 
